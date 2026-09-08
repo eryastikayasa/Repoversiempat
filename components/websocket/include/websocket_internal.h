d3d3b72101ae592ec8dedc7e6df1072d55ec3882
eryastikayasa <eryastikayasa@gmail.com>
2026-09-09 04:52:59 +0800

    Remove legacy playback completion declaration and typo alias

--- components/websocket/include/websocket_internal.h
+++ components/websocket/include/websocket_internal.h
@@ -21,8 +21,6 @@
 #define WS_TX_AUDIO_SIZE 3200
 #define WS_TX_QUEUE_LENGTH 3
 #define WS_RX_QUEUE_LENGTH WS_RX_SLOT_COUNT
-/* Temporary compatibility alias; remove after websocket_audio.cpp cleanup. */
-#define AUDIO_PLAYBACK_PLAYBACK_CRITICAL_SIZE AUDIO_PLAYBACK_CRITICAL_SIZE
 
 typedef enum { WS_TX_COMMAND_SETUP = 1, WS_TX_COMMAND_AUDIO = 2 } ws_tx_command_type_t;
 typedef struct { ws_tx_command_type_t type; uint32_t generation; uint16_t len; uint8_t *data; } ws_tx_command_t;
@@ -36,7 +34,7 @@ bool websocket_rx_init(void); void websocket_rx_request_reset(void); void websoc
  * a worker task so backpressure cannot stall the WebSocket client task. */
 bool websocket_rx_ingest_init(void); bool websocket_rx_ingest_enqueue(esp_websocket_event_data_t *data, uint32_t generation);
 extern esp_websocket_client_handle_t client; extern volatile bool is_connected; extern volatile bool setup_complete; extern volatile bool websocket_tx_error; extern char session_handle[SESSION_HANDLE_MAX_LEN]; extern bool session_resumable;
-extern StreamBufferHandle_t audio_stream; extern TaskHandle_t audio_playback_task_handle; extern volatile bool audio_turn_active; extern volatile bool audio_turn_complete_pending; extern volatile uint32_t websocket_connection_generation;
+extern StreamBufferHandle_t audio_stream; extern TaskHandle_t audio_playback_task_handle; extern volatile bool audio_turn_active; extern volatile uint32_t websocket_connection_generation;
 void websocket_schedule_setup(uint32_t generation);
 extern uint32_t audio_chunks_received; extern uint64_t audio_bytes_received; extern uint64_t audio_bytes_queued; extern uint32_t audio_write_calls; extern uint64_t audio_bytes_played; extern uint64_t audio_bytes_dropped;
 void websocket_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data); void reset_rx_buffer(void); bool ensure_rx_buffer(size_t required_size); void process_websocket_payload(esp_websocket_event_data_t *data); void process_gemini_message(const char *json, size_t len); bool build_gemini_setup(char **output, size_t *output_len); void clear_session_handle(void); bool store_session_handle(const char *handle); size_t get_audio_pending_bytes(void); bool start_audio_playback(void); void clear_audio_buffer(void); void request_audio_buffer_clear(void); void reset_audio_turn_stats(void); void begin_audio_turn(void); bool queue_audio_pcm(const uint8_t *pcm, size_t len); void check_audio_playback_complete(void);
