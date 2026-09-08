d41e5637c594dfe869c72e44722387d8f8a03579
eryastikayasa <eryastikayasa@gmail.com>
2026-09-08 18:30:21 +0800

    Update websocket_internal.h

--- components/websocket/include/websocket_internal.h
+++ components/websocket/include/websocket_internal.h
@@ -13,7 +13,7 @@
 #define WS_RX_SLOT_SIZE (40 * 1024)
 /* v7.0.31: increase persistent RX slot headroom after v7.0.30 showed
  * buffer_drop=4 and queue_hwm=4 during Gemini audio bursts. */
-#define WS_RX_SLOT_COUNT 8
+#define WS_RX_SLOT_COUNT 16
 #define WS_RX_TERMINATOR_SIZE 1
 #define WS_RX_STREAM_THRESHOLD (8 * 1024)
 #define WS_RX_STREAM_COMPACT_SIZE (8 * 1024)
