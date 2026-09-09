d4d6170c839548e8b90817c90df351420e580e7c
eryastikayasa <eryastikayasa@gmail.com>
2026-09-09 21:49:33 +0800

    display: expose duplicated text module API

--- components/display/display_text/display_text.h
+++ components/display/display_text/display_text.h
@@ -8,6 +8,13 @@ extern "C" {
 
 void display_text_init(void);
 void display_text_update(uint32_t now_ms);
+void display_text_set_user(const char *text);
+void display_text_set_gemini(const char *text);
+void display_text_append_user(const char *text);
+void display_text_append_gemini(const char *text);
+void display_text_render_user(void);
+void display_text_render_gemini(void);
+const uint8_t *display_text_buffer(void);
 
 #ifdef __cplusplus
 }
