c6af39c1ea4ece0eeb72c02f4432e6e915162c3a
eryastikayasa <eryastikayasa@gmail.com>
2026-09-09 21:20:49 +0800

    display: add display text skeleton

---
+++ components/display/display_text/display_text.cpp
@@ -0,0 +1,10 @@
+#include "display_text.h"
+
+void display_text_init(void) {
+    // Structure only. Existing text implementation is untouched.
+}
+
+void display_text_update(uint32_t now_ms) {
+    (void)now_ms;
+    // Structure only. Text behavior will be migrated here later.
+}
