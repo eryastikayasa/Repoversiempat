526caa18c101e97bf48dad4b0aff98b11d4948be
eryastikayasa <eryastikayasa@gmail.com>
2026-09-09 21:20:39 +0800

    display: add display face skeleton

---
+++ components/display/display_face/display_face.cpp
@@ -0,0 +1,10 @@
+#include "display_face.h"
+
+void display_face_init(void) {
+    // Structure only. Existing face implementation is untouched.
+}
+
+void display_face_update(uint32_t now_ms) {
+    (void)now_ms;
+    // Structure only. Face animation will be migrated here later.
+}
