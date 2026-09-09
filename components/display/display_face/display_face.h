8cbbe9b60d9dd46a987bf9143d7f0c299990524c
eryastikayasa <eryastikayasa@gmail.com>
2026-09-10 04:48:34 +0800

    Migrate timed face API into display_face

--- components/display/display_face/display_face.h
+++ components/display/display_face/display_face.h
@@ -26,6 +26,7 @@ typedef enum {
 void display_face_init(void);
 void display_face_update(uint32_t now_ms);
 void display_face_set_state(face_state_t state);
+void display_face_show_for_ms(face_state_t state, uint32_t duration_ms);
 face_state_t display_face_get_state(void);
 
 void display_face_render_mochi_gaze(int expr, int step,
