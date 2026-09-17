906648eb42fa7cdfe8be9cdb403cb3cc0115ec71
eryastikayasa <eryastikayasa@gmail.com>
2026-09-17 16:28:01 +0800

    Update audio_hal.cpp

--- components/audio_hal/audio_hal.cpp
+++ components/audio_hal/audio_hal.cpp
@@ -112,7 +112,7 @@ esp_err_t audio_hal_read_pcm(int16_t *buffer, size_t samples, size_t *samples_re
 
     size_t bytes_read = 0;
     const size_t input_bytes = samples * sizeof(int32_t);
-    esp_err_t err = i2s_channel_read(s_rx, s_rx_raw, input_bytes, &bytes_read, pdMS_TO_TICKS(100))
+    esp_err_t err = i2s_channel_read(s_rx, s_rx_raw, input_bytes, &bytes_read, pdMS_TO_TICKS(100));
     if (err != ESP_OK) return err;
 
     const size_t count = bytes_read / sizeof(int32_t);
